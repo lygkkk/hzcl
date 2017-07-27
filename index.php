7dce2f47e8944ea2bb505e7bc9bdef1b57fb38bf
lygkkk <lygkkk@163.com>
2017-07-27 16:24:28 +0800

    7-27-16-24

--- index.php
+++ index.php
@@ -3,13 +3,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scale=no">
-    <link href="/static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap/css/basic.css" rel="stylesheet">
+    <link href="bootstrap/css/index.css" rel="stylesheet">
 
-    <title>$Title$</title>
+    <title>首页</title>
 </head>
-<body>
- hzclmy
-<script src="/static/js/jquery.min.js"></script>
-<script src="/static/bootstrap/js/bootstrap.min.js"></script>
+<body class="container">
+ <div id = "header">
+      <h1>杭州春蓝贸易有限公司</h1>
+     <ul class="list-inline list-group-item-text">
+         <li>首页</li>
+         <li>注册</li>
+         <li>登录</li>
+         <li>暂无</li>
+         <li>暂无</li>
+     </ul>
+</div>
+
+ <div id = "body">
+    body
+ </div>
+
+ <div id = "footer">
+    footer
+ </div>
+<script src="bootstrap/js/jquery.min.js"></script>
+<script src="bootstrap/js/bootstrap.min.js"></script>
 </body>
 </html>
