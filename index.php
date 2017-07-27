7548e947527260e29f4315c647ed7ade0781197b
lygkkk <lygkkk@163.com>
2017-07-27 13:28:05 +0800

    07.27

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="zh-cn">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scale=no">
+    <link href="/static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+
+    <title>$Title$</title>
+</head>
+<body>
+ hzclmy
+<script src="/static/js/jquery.min.js"></script>
+<script src="/static/bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
